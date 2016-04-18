59903720da1fd9f7493b31ae891c58a240d3eb67
Ethan Raymond <ethan.raymond14@gmail.com>
2016-04-18 15:51:22 +0800

    gradle changes and settings activity

---
+++ app/src/main/java/hk/ust/cse/comp4521/taiotourism/SettingsActivity.java
@@ -0,0 +1,13 @@
+package hk.ust.cse.comp4521.taiotourism;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
