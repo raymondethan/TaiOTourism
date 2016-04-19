971419632b0db4901ec16d7ba44b8cbb5a62dddb
Loic Turin <turin.loic@gmail.com>
2016-04-19 15:03:12 +0800

    Small fixes on the drawer
    adding correct icons and texts.
    Modifying corresponding code in DrawerActivity.java

--- app/src/main/java/hk/ust/cse/comp4521/taiotourism/DrawerActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/taiotourism/DrawerActivity.java
@@ -80,17 +80,17 @@ public class DrawerActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_attractions) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_restaurants) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_map) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_credits) {
 
         }
 
