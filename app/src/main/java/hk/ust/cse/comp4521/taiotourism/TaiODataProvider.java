0cfcfb3b1c81cc1d2f039d87aa2b7ced72f3cb9d
Ethan Raymond <ethan.raymond14@gmail.com>
2016-04-25 16:12:18 +0800

    some progress on content provider

---
+++ app/src/main/java/hk/ust/cse/comp4521/taiotourism/TaiODataProvider.java
@@ -0,0 +1,65 @@
+package hk.ust.cse.comp4521.taiotourism;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import static hk.ust.cse.comp4521.taiotourism.TaiODataContract.*;
+
+/**
+ * Created by ethanraymond on 4/25/16.
+ */
+public class TaiODataProvider extends ContentProvider {
+
+    static final String PROVIDER_NAME = "hk.ust.cse.comp4521.taiotourism";
+    static final String URL = "content://" + PROVIDER_NAME + "/data";
+    static final Uri CONTENT_URI = Uri.parse(URL);
+
+    static final String _ID = "_id";
+    static final String NAME = "name";
+    static final String GRADE = "grade";
+
+    static final int POIEntry = 1;
+
+    static final UriMatcher uriMatcher;
+    static{
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER_NAME, "POIEntries", POIEntry);
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
