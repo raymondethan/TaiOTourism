a22a2a72e11855ee3a8adc6d66576ec17fa9d6cc
Ethan Raymond <ethan.raymond14@gmail.com>
2016-05-13 18:46:54 +0800

    location stored as coordinates

---
+++ app/src/main/java/hk/ust/cse/comp4521/taiotourism/syncAdapter/GeoPoint.java
@@ -0,0 +1,26 @@
+package hk.ust.cse.comp4521.taiotourism.syncAdapter;
+
+/**
+ * Created by ethanraymond on 5/13/16.
+ */
+public class GeoPoint {
+
+    private Double lat;
+    private Double lng;
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+}
