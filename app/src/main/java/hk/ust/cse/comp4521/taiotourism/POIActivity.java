1e830498bf34c35e3ba4efbc985ad33c26590418
Amanda Sambath <amanda.sambath@phelma.grenoble-inp.fr>
2016-05-15 15:49:29 +0800

    Update POIModel with picture uri field

---
+++ app/src/main/java/hk/ust/cse/comp4521/taiotourism/POIActivity.java
@@ -0,0 +1,13 @@
+package hk.ust.cse.comp4521.taiotourism;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by amanda on 15/05/16.
+ */
+public class POIActivity extends AppCompatActivity {
+
+    private String TAG = "POI Detailed View Activity";
+
+
+}
