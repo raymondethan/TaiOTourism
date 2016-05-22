56cf78b4d5b7ad7b93642abf96dc21342fd62d31
Loic Turin <turin.loic@gmail.com>
2016-05-22 23:09:15 +0800

    Adding name ID and email on every .java

--- app/src/main/java/hk/ust/cse/comp4521/taiotourism/syncAdapter/POIModel.java
+++ app/src/main/java/hk/ust/cse/comp4521/taiotourism/syncAdapter/POIModel.java
@@ -1,3 +1,8 @@
+//# COMP 4521    #  AMANDA SAMBATH                            20363161          asambath@connect.ust.hk
+//# COMP 4521    #  ETHAN RAYMOND                              20363147          eoraymond@connect.ust.hk
+//# COMP 4521    #  NICKOLAS JARZEMBOWSKI               2033 8324          njj@connect.ust.hk
+//# COMP 4521    #  LOIC BRUNO STEPAHNE TURIN        20363264          lbsturin@connect.ust.hk
+
 package hk.ust.cse.comp4521.taiotourism.syncAdapter;
 
 import android.net.Uri;
